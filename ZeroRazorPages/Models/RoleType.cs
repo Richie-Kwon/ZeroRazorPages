9ac0e2abf1a03a26c69ae32cad5bffcfe7e6b95b
richie <richie.kwon1@gmail.com>
2022-03-16 21:04:57 +0000

    crud first migration

---
+++ ZeroRazorPages/Models/RoleType.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ZeroRazorPages.Models
+{
+    [Table("RolesTypes")]
+    public class RoleType
+    {
+        [Key] 
+        public int Id { get; set; }
+        
+        [Display(Name = "Role Name")]
+        public string Name { get; set; }
+
+        public bool Active { get; set; }
+    }
+}
