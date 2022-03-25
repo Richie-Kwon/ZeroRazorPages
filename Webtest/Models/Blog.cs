88f4acf3ebb7bfff5f99a518d9040f228c10a9e9
richie <richie.kwon1@gmail.com>
2022-03-25 08:49:26 +0000

    cors

--- Webtest/Models/Blog.cs
+++ Webtest/Models/Blog.cs
@@ -10,14 +10,4 @@ namespace Webtest.Models
         public string Name  { get; set; }
         public ICollection<Post> Posts { get; set; }
     }
-
-    [Table("Posts")]
-    public class Post
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public string Content { get; set; }
-        public Blog Blog { get; set; }
-        public int BlogId { get; set; }
-    }
-}
+}   
