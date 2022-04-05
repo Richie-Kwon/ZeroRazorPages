20ef420c1202417a8865a22e21723aaea381b3ea
richie <richie.kwon1@gmail.com>
2022-04-05 19:29:15 +0100

    file uploading

--- APICRUD/Controllers/FileUploadController.cs
+++ APICRUD/Controllers/FileUploadController.cs
@@ -1,9 +1,15 @@
 using System;
 using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.IO;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using Microsoft.VisualStudio.Web.CodeGeneration.Contracts.Messaging;
 
 namespace APICRUD.Controllers
 {
@@ -11,14 +17,33 @@ namespace APICRUD.Controllers
     [ApiController]
     public class FileUploadController : ControllerBase
     {
-       
+        private readonly IWebHostEnvironment _environment;
+
+        public FileUploadController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+
 
         // POST: api/FileUpload
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<IActionResult> Post([FromBody] List<IFormFile> files)
         {
-        }
+            var uploadFolder = Path.Combine(_environment.WebRootPath, "files");
 
-        
-    }
+            foreach (var file in files)
+                if (file.Length > 0)
+                {
+                    var fileName = Path.GetFileName(ContentDispositionHeaderValue.Parse(file.ContentDisposition)
+                        .FileName.Trim('"'));
+
+                    using (var fileStream = new FileStream(Path.Combine(uploadFolder, fileName), FileMode.Create))
+                    {
+                        await file.CopyToAsync(fileStream);
+                    }
+                }
+
+            return Ok( new { message = "OK"});
+        }
+    } 
 }
